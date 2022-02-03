1829fdc5526f65e3663ef8e9cdbd71272c6a075e
YihengJIAN <jianyiheng@outlook.com>
2022-02-03 01:40:10 -0800

    enable cache for keeping date when refresh

--- static/js/note.js
+++ static/js/note.js
@@ -5,22 +5,121 @@ const app = Vue.createApp({
   data() {
     return {
       note: {
+        noteid: '',
         title: '',
-        text: ''
+        body: '',
+        updateDate: '',
+        editStatus: false,
       },
       newnote: {
-        title: 'New Title',
-        body: 'New Note'
+        noteid: '',
+        title: '',
+        body: '',
+        updateDate: '',
+        editStatus: false,
       },
-      notes: []
+      notes: [
+        {
+          noteid: '01010011',
+          title: 'demo title',
+          body: 'demo body',
+          updateDate: 'demo date',
+          editStatus: false,
+        }
+      ]
     };
   },
+  mounted: function () {
+    this.initNote();
+  },
   methods: {
+    display(value) {
+      console.log(value);
+    },
+    getIdbyIndex (index) {
+      if (index==-1) {
+        return this.newnote.noteid;
+      } else {
+        return this.notes[index].noteid;
+      }
+    },
+    initNote () {
+      for (const note of this.notes) {
+        if (sessionStorage.getItem(note.noteid)) {
+          const savenote = JSON.parse(sessionStorage.getItem(note.noteid));
+          note.title = savenote.title;
+          note.body = savenote.body;
+          note.updateDate = savenote.udpateDate;
+        }
+      }
+    },
+    existTemp (index) {
+      var noteid;
+      if (index==-1) {
+        noteid = this.newnote.noteid;
+      } else {
+        noteid = this.notes[index].noteid;
+      }
+      console.log(noteid);
+      if (sessionStorage.getItem(noteid)) {
+        console.log('yes');
+        return true;
+      } else {
+        console.log('no');
+        return false;
+      }
+    },
+    saveTempTitle (value, initialValue, index) {
+      const date = new Date();
+      var body, noteid;
+      if (index==-1) {
+        this.newnote.editStatus = true;
+        body = this.newnote.body;
+        noteid = this.newnote.noteid;
+      } else {
+        this.notes[index].editStatus = true;
+        body = this.notes[index].body;
+        noteid = this.notes[index].noteid;
+      }
+      sessionStorage.setItem(noteid, JSON.stringify({
+        title: value,
+        body: body,
+        updateDate: date.getTime()
+      }));
+    },
+    saveTempBody (value, initialValue, index) {
+      const date = new Date();
+      var title, noteid;
+      if (index==-1) {
+        this.newnote.editStatus = true;
+        title = this.newnote.title;
+        noteid = this.newnote.noteid;
+      } else {
+        this.notes[index].editStatus = true;
+        title = this.notes[index].title;
+        noteid = this.notes[index].noteid;
+      }
+      sessionStorage.setItem(noteid, JSON.stringify({
+        title: title,
+        body: value,
+        updateDate: date.getTime()
+      }));
+    },
+    loadTemp (index) {
+      var noteid;
+      if (index==-1) {
+        noteid = this.newnote.noteid;
+      } else {
+        noteid = this.notes[index].noteid;
+      }
+
+      return JSON.parse(sessionStorage.getItem(noteid));
+    },
     newNote() {
       let {
         title,
         body
-      } = this.newnote
+      } = this.newnote;
       
       this.notes.push({
         title,
@@ -30,47 +129,6 @@ const app = Vue.createApp({
       this.newnote.title = "New Title";
       this.newnote.body = "New Note";
 
-      axios.post('/update', {
-        title: title,
-        body: body,
-        index: 0 
-      })
-      .then(function (response) {
-        console.log(response.data);
-        return response.data;
-      })
-      .catch(function (error) {
-        alert(error.data);
-      })
-    },
-    updateNote(index) {
-      let {
-        title,
-        body
-      } = this.notes[index]
-
-      // axios.post('/update', {
-      //   title: title,
-      //   body: body,
-      //   index: index
-      // })
-      axios({
-        method:'post',
-        url: '/update',
-        data:{
-          title: title,
-          body: body,
-          index: index,
-        },
-        contentType:'application/x-www-form-urlencoded'
-      })
-      .then(function (response) {
-        console.log(response.data);
-        return response.data;
-      })
-      .catch(function (error) {
-        alert(error.data);
-      });
     },
     removeNote(index) {
       this.notes.splice(index, 1);
