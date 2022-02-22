37cfc495c9f549ec79ac0a7429f20c487caf8b5c
xushe <503455782@qq.com>
2022-02-22 11:38:02 -0800

    Support the function of keyword extraction and note storage (in-memory)

---
+++ keyword_extraction.py
@@ -0,0 +1,166 @@
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import treebank
+from collections import defaultdict
+
+
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import PorterStemmer
+
+
+
+def extract_from_para(note, top_k=5):
+    stop_words = set(stopwords.words('english'))
+    extract_for_all_paragraphs = defaultdict(int)
+
+    sen_tokenize_res = sent_tokenize(note.body)
+    filter_list = {'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday',
+                   'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'}
+
+
+    for sentence in sen_tokenize_res:
+        tokenizer = RegexpTokenizer(r'\w+')
+        word_res = tokenizer.tokenize(sentence)
+
+        filtered_sentence = []
+
+        for word in word_res:
+            if word not in stop_words or word.isalpha():
+                filtered_sentence.append(word.strip())
+        # print(sentence)
+
+        tagged = nltk.pos_tag(word_res)
+        entities = nltk.chunk.ne_chunk(tagged)
+        # print(entities)
+
+        extracted = []
+        NER = []
+        for i in range(len(entities)):
+            if type(entities[i]) is not nltk.Tree:
+                a, b = entities[i]
+            else:
+                a, b = entities[i][0]
+
+            cmp_key = a.lower()
+            if cmp_key in filter_list:
+                continue
+
+            if b == 'NN' and len(a) > 1:
+                extracted.append(a)
+            elif b == 'NNP' and len(a) > 1:
+                NER.append(a)
+
+        final_res = NER + extracted
+        # print('Extracted Keywords: ', ' '.join(final_res))
+        # print()
+        for word in final_res:
+            extract_for_all_paragraphs[word] += 1
+
+
+    extract_for_all_paragraphs = list(extract_for_all_paragraphs.items())
+    extract_for_all_paragraphs = sorted(extract_for_all_paragraphs, key=lambda x: -x[1])
+    # print('\n' * 5)
+
+    ret = []
+    for i in range(min(top_k, len(extract_for_all_paragraphs))):
+        # print(extract_for_all_paragraphs[i][0])
+        ret.append(extract_for_all_paragraphs[i][0])
+    return ret
+
+
+
+
+    
+
+
+class Note:
+    
+    def __init__(self, noteId, title, body, keywords, updateDate):
+        self.noteId = noteId
+        self.title = title
+        self.body = body
+        self.keywords = keywords
+        self.updateDate = updateDate
+
+id_note = {}
+keyword_note = {}
+def save_or_update_note(note):
+    id_note[note.noteId] = note
+
+
+def save_or_update_keywords(note, keywords):
+    note.keywords = []
+    for keyword in keywords:
+        note.keywords.append(keyword)
+        if keyword in keyword_note:
+            id_notes = keyword_note.get(keyword)
+            id_notes[note.noteId] = note
+        else:
+            id_notes = {}
+            id_notes[note.noteId] = note
+            keyword_note[keyword] = id_notes
+
+def save_note_and_keywords(note):
+    save_or_update_note(note)
+    keywords = extract_from_para(note)
+    save_or_update_keywords(note, keywords)
+           
+def search(keyword):
+    notes = []
+    notes = id_note.values()
+    res = []
+    for note in notes:
+        ans = note.body.find(keyword)
+        if (ans != -1):
+            res.append(note)
+    return res
+
+
+
+
+if __name__ == '__main__':
+    # example_note = 'Want to leave an important message to someone? Or record an essential detail that you don’t want to forget? Then you need not worry about whether to write a short informal letter or written message to a particular person or to an organization. You can even customize or decorate it for a more personalized touch.'
+
+    # example_note = "Jim is the one who plays footable for the USA. He even played in the FIFA."
+
+    example_note = '''
+Open-access papers have drastically fewer lead authors from low-income regions than do paywalled articles, an analysis of tens of thousands of articles shows. The findings suggest that the fees that journals charge to publish articles open access pose a barrier for authors in low- and middle-income countries — something that scientists had previously suspected but found difficult to demonstrate.
+
+Increasing numbers of scholarly journals — including Nature — have been making their articles open access, driven in part by requirements from funders (Nature’s news team is editorially independent of its publisher Springer Nature). Although this shift has been making the scholarly literature more widely accessible, many researchers have noted that the article-processing charges (APCs) typically required to publish research open access can deter authors from using this option.
+
+“One of the great ironies of open access is that you grant authors around the world the ability to finally read the scientific literature that was completely closed off to them, but it ends up excluding them from publishing in the same journals,” says Emilio Bruna, an ecologist and scholar of Latin American studies at the University of Florida in Gainesville.
+
+Although many in the scientific community recognize this, it’s been a challenge to demonstrate it empirically, says Bruna. One problem is the difficulty of directly comparing open-access and non-open-access journals, because even those from the same publisher might differ in factors such as reputation and standards of acceptance.
+
+The Dutch publisher Elsevier’s ‘mirror journals’ pilot project, which ran from 2018 to 2020, offered Bruna’s team an opportunity. Under this programme, existing hybrid journals, which contain both paywalled and freely accessible articles, shared titles and editorial and peer-review processes with fully open-access versions, termed mirror journals. “It’s as close as you can get to a natural experiment,” Bruna says.
+
+His group examined more than 37,000 articles published in 38 pairs of mirror and ‘parent’ journals, which mostly published paywalled content. The researchers identified the geographical location of each paper’s first author, then looked up the country’s World Bank income category and whether the author qualified for a full or partial APC waiver under Elsevier’s Research4Life programme for authors from certain countries. The APCs for the mirror journals cost a median of US$2,600; most of the hybrid journals had APCs identical to those of their mirror journals.
+Global divide
+
+Overall, mirror journals had more articles with lead authors from North America, East Asia and the Pacific region than did paywalled articles in their corresponding parent versions. They had fewer lead authors from lower-income areas, in particular those in the global south, such as Latin America and the Caribbean, the Middle East and North Africa, and sub-Saharan Africa. Around 80% of articles in mirror journals had first authors from high-income countries, and none had first authors from low-income countries.
+
+“When we see results like this, it just makes it crystal clear that there is a very strong financial barrier for publishing when journals charge APCs,” says Rafael Zenni, an ecologist at the Federal University of Lavras in Brazil. “It’s something that we face every day. In our research grants, there are rarely any resources for publishing fees.”
+
+The team also found that authors based in countries eligible for the waiver programme almost never published open-access articles. Bruna was surprised by how ineffective waivers seemed to be. And when waivers are used, even large discounts don’t reduce the cost enough for authors from lower-income regions, who often pay APCs out of their own pockets.
+
+“It’s widely known that APCs deter authors without means, but this particular method of quantifying the economic barrier to these authors is new and I think it should be persuasive,” says Peter Suber, director of the Harvard Office for Scholarly Communication in Cambridge, Massachusetts. “APCs distort research they exclude authors, and we should all be trying to find ways to overcome those barriers,” says Suber.
+
+Elsevier declined the Nature news team’s request for comment on this study.
+'''
+
+    #print(extract_from_para(example_note, 15))
+    note = Note(10, "sdad", example_note, None, 2000)
+    save_note_and_keywords(note)
+    note2 = Note(1, "sad", "The", None, 23)
+    save_or_update_note(note2)
+   # print(keyword_note.get("income").get(10).body)
+
+    search_res = search("The")
+    print(search_res[0].body)
+    
+
+
+
